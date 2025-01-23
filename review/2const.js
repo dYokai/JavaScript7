5a79c9f247c6234c0161edda52c0f5d246158441
D. Anagnostopoulos <dimitris_anagn@hotmail.com>
2025-01-23 19:55:37 +0200

    javascript intro and review

---
+++ review/2const.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+
+const PI = 3.14
+const maxThreads = 150
+let counter = 0
+counter++
+
+console.log(PI, typeof PI, maxThreads, counter)
---
+++ review/3d-boolean.js
@@ -0,0 +1,10 @@
+let isDarkMode = false
+let username = ""
+let isUserLoggedIN = false
+let isDashboardOn = false
+console.log(`Is dark mode enabled ${isDarkMode}`)
+
+isUserLoggedIN = "true"
+username = "Anna"
+isDashboardOn = isUserLoggedIN && username
+console.log(isDashboardOn)
